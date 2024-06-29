eb1d7806b5fa45a3e90cd885dcd41bd7368c3c58
ycraah <ycraah@gmail.com>
2024-06-29 19:57:42 +0900

    게시물 등록write입력하면 게시물 제목, 내용 입력받기

--- lecture/src/main/java/com/ycraah/javaboard/Main.java
+++ lecture/src/main/java/com/ycraah/javaboard/Main.java
@@ -5,16 +5,33 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     System.out.println("==프로그램 시작==");
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in); //스캐너
+    int id = 0; //게시물 번호
 
     while(true){
       System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equalsIgnoreCase("exit")) break;
+      if(cmd.equalsIgnoreCase("write")){
+        ++id;
+        System.out.println("게시물 등록");
+        System.out.print("제목) ");
+        String title = sc.nextLine();
+
+        System.out.print("내용) ");
+        String content = sc.nextLine();
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      } else if(cmd.equalsIgnoreCase("exit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }
+      else {
+        System.out.println("잘못된 명령어 입니다.");
+      }
     }
 
-    sc.close();
+    sc.close(); //스캐너 종료
     System.out.println("==프로그램 종료==");
   }
 }
