7f1586119a79407dcd4e8da1d609849fd9cc24b7
ycraah <ycraah@gmail.com>
2024-06-29 19:51:58 +0900

    exit을 입력받으면 종료

--- lecture/src/main/java/com/ycraah/javaboard/Main.java
+++ lecture/src/main/java/com/ycraah/javaboard/Main.java
@@ -1,7 +1,20 @@
 package com.ycraah.javaboard;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("시작");
+    System.out.println("==프로그램 시작==");
+    Scanner sc = new Scanner(System.in);
+
+    while(true){
+      System.out.print("명령) ");
+      String cmd = sc.nextLine();
+
+      if(cmd.equalsIgnoreCase("exit")) break;
+    }
+
+    sc.close();
+    System.out.println("==프로그램 종료==");
   }
 }
