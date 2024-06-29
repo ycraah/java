9fcb237b9dcdd0dfb6138bb8e7ecf84bd420c4c2
ycraah <ycraah@gmail.com>
2024-06-29 20:39:39 +0900

    detail을 입력하면 최근 게시물의 상세 정보 출력

--- lecture/src/main/java/com/ycraah/javaboard/Main.java
+++ lecture/src/main/java/com/ycraah/javaboard/Main.java
@@ -9,6 +9,7 @@ public class Main {
     System.out.println("==프로그램 시작==");
     Scanner sc = new Scanner(System.in); //스캐너
     int id = 0; //게시물 번호
+    int lastId = 0;
     List<Article> articles = new ArrayList<>(); //게시물 번호, 제목, 내용
 
     while(true){
@@ -23,15 +24,26 @@ public class Main {
 
         System.out.print("내용) ");
         String content = sc.nextLine();
-        Article article = new Article(title, content, id);
-        articles.add(article);
+        Article article = new Article(title, content, id); // 게시판 정보를 담은 article 형성
+        articles.add(article); //article을 articles에 저장
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
         System.out.println(article.toString());
+
+        lastId = id;
+
       } else if(cmd.equalsIgnoreCase("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break;
+
+      } else if(cmd.equalsIgnoreCase("detail")){
+        System.out.println("-게시물 상세보기-");
+
+        Article lastArticle = articles.get(lastId-1); //lastId에 의거해서 마지막 게시판 정보 불러오기
+
+        System.out.printf("번호 : %d \n제목 : %s \n내용 : %s\n", lastArticle.id, lastArticle.title, lastArticle.content);
       }
+
       else {
         System.out.println("잘못된 명령어 입니다.");
       }
